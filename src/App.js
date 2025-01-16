2b938f91880506df989682ab7be382a2c3a19bb2
priyanshibansal663 <priyanshibansal663@gmail.com>
2025-01-16 16:16:11 +0530

    why us and contactUs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import Header from "./components/Header";
 import AboutUs from "./pages/AboutUs";
 import Home from "./pages/Home";
 import ContactUs from "./pages/ContactUs";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AppLayout from "./components/AppLayout/AppLayout";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import WhyUs from "./pages/WhyUs";
 
 function App() {
   const router = createBrowserRouter([
@@ -23,6 +26,10 @@ function App() {
           path: "/ContactUs",
           element: <ContactUs />,
         },
+        {
+          path: "/WhyUs",
+          element: <WhyUs />,
+        }
       ],
     },
   ]);
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Footer = () => {
+  return (
+    <div>
+      
+      gcuygducsy
+      
+    </div>
+  )
+}
+
+export default Footer;
