01b27e7e7a252685ce1e60fa3b67aee5ff83d0d6
RinkuMonu <rinkuyadav9460@gmail.com>
2025-01-18 11:18:12 +0530

    popup  add

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 
+import { useState } from 'react';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Header from "./components/Header/Header.js";
 import AboutUs from "./pages/AboutUs/AboutUs.js";
 import Home from "./pages/Home";
 import ContactUs from "./pages/ContactUs";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AppLayout from "./components/AppLayout/AppLayout";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import OurService from "./pages/OurService/OurService.js";
 import WhyUs from "./pages/WhyUs";
 import BillPayUI from '../src/pages/services/BillPayUI.js'
 import OurPartner from "./pages/OurPartner";
 import BBPS from "./pages/BBPS";
+import Login from "./pages/Login/Login";
+import Popup from "./pages/Popup/Popup.jsx";
 
 function App() {
+ 
   const router = createBrowserRouter([
     {
       path: "/",
@@ -52,13 +56,15 @@ function App() {
         {
           path: "/BBPS",
           element: <BBPS />,
-        }
+        },
+       
       ],
     },
   ]);
   return (
     <>
       <RouterProvider router={router} />
+     
     </>
   );
 }
